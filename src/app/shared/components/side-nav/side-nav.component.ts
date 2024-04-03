5c743795a2c3f4b1f28b75b323bf0e66361f2b15
JefersonNobuntu <jefersoncmnn@gmail.com>
2024-04-03 15:33:08 -0300

    fix: SideNavBar over content on load

--- src/app/shared/components/side-nav/side-nav.component.ts
+++ src/app/shared/components/side-nav/side-nav.component.ts
@@ -1,8 +1,15 @@
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable, forkJoin, map, shareReplay, take } from 'rxjs';
+import { environment } from 'enviroment/environment';
+import { Observable, map, shareReplay, take, tap } from 'rxjs';
 
+/**
+ * Interface que contém informações das opções de vavegação do sideNavBar
+ * @param routeUrl Rota que o usuário será levado ao selecionar a opção
+ * @param optionName Nome da opção. @example "Product"
+ * @param svgIcon Caminho para o icone da opcão. @example "feather:box"
+ */
 interface INavListOption {
   routeUrl: string,
   optionName: string,
@@ -15,23 +22,61 @@ interface INavListOption {
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent {
-  navListOptions : INavListOption[] | null;
+  /**
+   * Opções que são apresentadas na lista lateral para navegar para outras paginas da aplicação
+   */
+  navListOptions: INavListOption[] | null;
+  /**
+   * Título que será apresentada no cabeçário da aplicação.
+   */
+  applicationTitle: string = environment.applicationTitle;
+  /**
+   * Texto que aparecerá no topo da navegador lateral.
+   */
+  menuTitle: string = 'Menu';
 
-  applicationTitle : string = 'NadirDigital'; 
-  menuTitle : string = 'Menu';
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset) 
-  .pipe( 
-    map(result => result.matches), 
-    shareReplay() 
-  ); 
+  isHandset$: Observable<boolean>;
+  /**
+   * Define se o sideNavBat (navegador lateral) está aberto ou não,
+   */
+  sideNavBarIsOpened: boolean = false;
+  /**
+   * Define se é permitido fechar a sideNavBar (navegador lateral).
+   */
+  canCloseNavBar: boolean = true;
 
   constructor(private breakpointObserver: BreakpointObserver, private httpClient: HttpClient) {
 
     this.getDataToMenu("/assets/dicionario/menu.json").then(data => {
       this.navListOptions = data;
+
+      this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+        .pipe(
+          map(result => result.matches),
+          tap(result => {
+            //Se o resultado for falso (for tela grande)
+            if (result == false) {
+              this.canCloseNavBar = false;//Não poderá fechar o NavBar
+              this.sideNavBarIsOpened = true;//Deixará o NavBarAberto
+            } else {
+              this.canCloseNavBar = true;
+              this.sideNavBarIsOpened = false;
+            }
+          }),
+          shareReplay()
+        );
     });
 
+  }
+
+  showSideNavBar() {
+    this.sideNavBarIsOpened = true;
+  }
+
+  closeSideNavBar() {
+    if (this.canCloseNavBar == true) {
+      this.sideNavBarIsOpened = false;
+    }
   }
 
   /**
@@ -40,25 +85,25 @@ export class SideNavComponent {
    * @returns Retorna um array com informações para criar o menu de navegação.
    */
   getDataToMenu(JSONMenuPath: string): Promise<INavListOption[]> {
-    
+
     return new Promise<INavListOption[]>((resolve, reject) => {
-      
+
       this.httpClient.get<any>(JSONMenuPath).pipe(take(1)).subscribe({
         next: (data) => {
-          let navListOptions : INavListOption[] = [];
+          let navListOptions: INavListOption[] = [];
 
-          if(('itens' in data) == false) return;
+          if (('itens' in data) == false) return;
 
           data.itens.forEach(item => {
-            navListOptions.push({optionName: item.name, svgIcon: item.icon, routeUrl: item.routeUrl })
+            navListOptions.push({ optionName: item.name, svgIcon: item.icon, routeUrl: item.routeUrl })
           });
-          
+
           resolve(navListOptions);
 
         },
         error: (error) => {
           console.warn(error);
-          
+
           reject(error);
         }
       });
