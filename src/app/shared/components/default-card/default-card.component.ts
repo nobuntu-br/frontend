bd936a0cbaf3206c7168019226f76d187e86565a
Jospiado <bugsescripts@hotmail.com>
2024-09-18 21:03:06 -0300

    add: component de mudanças do modo de exibição e respectiva mudança no css

--- src/app/shared/components/default-card/default-card.component.ts
+++ src/app/shared/components/default-card/default-card.component.ts
@@ -1,14 +1,21 @@
-import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { DateFieldComponent } from '../date-field/date-field.component';
 import { FieldComponent } from '../field/field.component';
 
 @Component({
   selector: 'app-default-card',
   templateUrl: './default-card.component.html',
-  styleUrls: ['./default-card.component.scss']
+  styleUrls: ['./default-card.component.scss'],
 })
 export class DefaultCardComponent implements AfterViewInit {
-
   @Input() itemDisplayed: any;
   /**
    * Nome da classe na qual o formulário pertence
@@ -18,7 +25,7 @@ export class DefaultCardComponent implements AfterViewInit {
   @Input() displayedfieldsName: string[];
   @Input() fieldsType: string[];
   /**
-   * Caso o campo for um objeto, será o nome do campo que está dentro do que será exibido. 
+   * Caso o campo for um objeto, será o nome do campo que está dentro do que será exibido.
    * [Exemplo]: O campo tem um objeto, esse objeto tem "id", "name" e "age". O campo apresentado poderá ser o "name", assim aparecerá o valor do campo "name" no componente.
    */
   @Input() objectDisplayedValue: string | null;
@@ -28,18 +35,16 @@ export class DefaultCardComponent implements AfterViewInit {
   @Output() eventOnSelect = new EventEmitter<void>();
   @Output() eventClickToEdit = new EventEmitter<void>();
 
-
   columnsQuantityStyle;
   isSelected: boolean = false;
 
+  @ViewChild('placeToRender', { read: ViewContainerRef })
+  target!: ViewContainerRef;
 
-  @ViewChild('placeToRender', { read: ViewContainerRef }) target!: ViewContainerRef;
-
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.createComponentsOnView();
-
   }
 
   get customStyle(): any {
@@ -65,27 +70,33 @@ export class DefaultCardComponent implements AfterViewInit {
   /**
    * Cria o componente que irá apresentar as informações do item
    * @param target Referência onde deve ser criado o novo componente na tela
-   * @param fieldType Tipo de campo que é a variável do item. Se é uma variável de data, texto ou outras opções. 
+   * @param fieldType Tipo de campo que é a variável do item. Se é uma variável de data, texto ou outras opções.
    * [Exemplo]: "date", "text"
-   * @param value Valor do campo. 
-   * [Exemplo]: O campo é "phone" e o valor é "9999999999" ou "nascimento" e o valor é "12/12/20" 
-   * @param labelTittle Titulo que o campo irá apresentar. 
+   * @param value Valor do campo.
+   * [Exemplo]: O campo é "phone" e o valor é "9999999999" ou "nascimento" e o valor é "12/12/20"
+   * @param labelTittle Titulo que o campo irá apresentar.
    * [Exemplo]: O nome do campo é "phone", então esse valor será apresentado para informar o valor do que está sendo apresentado.
-   * @param objectDisplayedValue Caso o campo for um objeto, será o nome do campo que está dentro do que será exibido. 
+   * @param objectDisplayedValue Caso o campo for um objeto, será o nome do campo que está dentro do que será exibido.
    * [Exemplo]: O campo tem um objeto, esse objeto tem "id", "name" e "age". O campo apresentado poderá ser o "name", assim aparecerá o valor do campo "name" no componente.
    */
-  createComponent(target: ViewContainerRef, fieldType, value, labelTittle: string, objectDisplayedValue: string | null) {
+  createComponent(
+    target: ViewContainerRef,
+    fieldType,
+    value,
+    labelTittle: string,
+    objectDisplayedValue: string | null
+  ) {
     if (target == null) {
-      console.error("Target not renderized in DefaultCard");
+      console.error('Target not renderized in DefaultCard');
       return;
     }
 
     let componentCreated;
 
     switch (fieldType) {
-
       case 'date': {
-        componentCreated = this.target.createComponent(DateFieldComponent).instance;
+        componentCreated =
+          this.target.createComponent(DateFieldComponent).instance;
         componentCreated.format = this.getFormatDateFromUserConfig();
         componentCreated.value = value;
         break;
@@ -94,11 +105,11 @@ export class DefaultCardComponent implements AfterViewInit {
         componentCreated = this.createObjectField(objectDisplayedValue, value);
         break;
       }
-      case 'object':{
+      case 'object': {
         componentCreated = this.createObjectField(objectDisplayedValue, value);
         break;
       }
-      case 'subform':{
+      case 'subform': {
         componentCreated = this.createObjectField(objectDisplayedValue, value);
         break;
       }
@@ -107,7 +118,6 @@ export class DefaultCardComponent implements AfterViewInit {
         componentCreated.value = value;
         break;
       }
-
     }
 
     componentCreated.label = labelTittle;
@@ -117,75 +127,80 @@ export class DefaultCardComponent implements AfterViewInit {
   /**
    * Cria um campo que apresentará um objeto ou vários, apresentando somente um campo especificado
    * @param objectDisplayedValue Especificará o campo o objeto(s) que será apresentado. [Exemplo]: No objeto tem o campo "name", então ele será apresentado.
-   * @param object Objeto que contém os campos e valores, sendo que um desses campos serão apresentados 
+   * @param object Objeto que contém os campos e valores, sendo que um desses campos serão apresentados
    */
-  createObjectField(objectDisplayedValue: string, object: object){
-    if (objectDisplayedValue == null || objectDisplayedValue == "") {
-      console.error("Campo de chave estrangeira não especificou o campo do objeto que será apresentado");
+  createObjectField(objectDisplayedValue: string, object: object) {
+    if (objectDisplayedValue == null || objectDisplayedValue == '') {
+      console.error(
+        'Campo de chave estrangeira não especificou o campo do objeto que será apresentado'
+      );
       return;
     }
 
-    let componentCreated : FieldComponent = this.target.createComponent(FieldComponent).instance;
+    let componentCreated: FieldComponent =
+      this.target.createComponent(FieldComponent).instance;
 
     //Se não tiver valor, será apresentado um valor fixo
     if (object == null) {
-      componentCreated.value = " - ";
+      componentCreated.value = ' - ';
       return componentCreated;
-    } 
+    }
 
     //Verificar se tem a propriedade do valor que será apresentado
 
-
-    let displayedValue : string;
+    let displayedValue: string;
 
     //Verifica se é um array
-    if(object instanceof Array){
-      if(object.length == 0){
-        componentCreated.value = " - ";
+    if (object instanceof Array) {
+      if (object.length == 0) {
+        componentCreated.value = ' - ';
         return componentCreated;
       }
-      if(object[0].hasOwnProperty(objectDisplayedValue) == false){
+      if (object[0].hasOwnProperty(objectDisplayedValue) == false) {
         objectDisplayedValue = Object.getOwnPropertyNames(object[0])[0];
-        displayedValue = object.map((item: any) => item[objectDisplayedValue]).join(", ");
+        displayedValue = object
+          .map((item: any) => item[objectDisplayedValue])
+          .join(', ');
       } else {
-        displayedValue = object.map((item: any) => item[objectDisplayedValue]).join(", ");
+        displayedValue = object
+          .map((item: any) => item[objectDisplayedValue])
+          .join(', ');
       }
     } else {
       if (object.hasOwnProperty(objectDisplayedValue) == false) {
-      //Caso o nome não for encontrado do campo, pega o promeiro
-      const objectPropertyNames = Object.getOwnPropertyNames(object);
-      objectDisplayedValue = objectPropertyNames[0];
-    }
+        //Caso o nome não for encontrado do campo, pega o promeiro
+        const objectPropertyNames = Object.getOwnPropertyNames(object);
+        objectDisplayedValue = objectPropertyNames[0];
+      }
       displayedValue = object[objectDisplayedValue];
     }
-    
+
     componentCreated.value = displayedValue;
 
     return componentCreated;
-
   }
 
   getFormatDateFromUserConfig() {
     if (this.userConfig == null) {
-      return "dd/MM/YY"
+      return 'dd/MM/YY';
     }
 
     if (this.userConfig.dateFormat != null) {
       return this.userConfig.dateFormat;
     } else {
-      return "dd/MM/YY"
+      return 'dd/MM/YY';
     }
   }
 
   getLanguageDateFromUserConfig() {
     if (this.userConfig == null) {
-      return "en"
+      return 'en';
     }
 
     if (this.userConfig.dateFormat != null) {
       return this.userConfig.language;
     } else {
-      return "en"
+      return 'en';
     }
   }
 
@@ -197,9 +212,7 @@ export class DefaultCardComponent implements AfterViewInit {
     this.eventOnSelect.emit(this.itemDisplayed);
   }
 
-  onClickToEdit(){
+  onClickToEdit() {
     this.eventClickToEdit.emit(this.itemDisplayed);
   }
-
 }
-
